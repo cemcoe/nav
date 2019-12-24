b29dd5c56212c193ca5009685d514ca72c8760b8
cemcoe <chemxcode@gmail.com>
2019-12-24 22:38:59 +0800

    divi json

--- src/js/Home/components/coolzhan.js
+++ src/js/Home/components/coolzhan.js
@@ -1,46 +1,12 @@
-const CoolZhanListFirst = [{
-    url: "https://www.dcard.tw/f",
-    name: "Dcard"
-}, {
-    url: "https://aotu.io/cates/Web%E5%BC%80%E5%8F%91/index.html",
-    name: "aoto"
-}, {
-    url: "https://learn.co/tracks/bootcamp-prep-v2",
-    name: "prep"
-}, {
-    url: "https://magi.com/",
-    name: "magi"
-}]
+const {
+    CoolZhanListFirst,
+    CoolZhanListSecond,
+    CoolZhanListThird } = require('./data/coolzhan.json')
 
-const CoolZhanListSecond = [{
-    url: "https://m.weibo.cn/",
-    name: "weibo"
-}, {
-    url: "https://stackoverflow.com/",
-    name: "SF"
-}, {
-    url: "https://www.okzy.co/?m=vod-type-id-3.html",
-    name: "ok"
-}, {
-    url: "https://www.freecodecamp.org/",
-    title: "The Home of the Hacker - Malware, Reverse Engineering, and Computer Science",
-    name: "camp"
-}]
-
-const CoolZhanListThird = [{
-    url: "https://learn-anything.xyz/",
-    name: "anything"
-}, {
-    url: "https://bbs.nga.cn/",
-    name: "Nga"
-}, {
-    url: "https://qiita.com/",
-    name: "Qiita"
-}, {
-    url: "https://hacpai.com/",
-    name: "黑派"
-}]
-
+console.log(
+    CoolZhanListFirst,
+    CoolZhanListSecond,
+    CoolZhanListThird)
 
 const coolzhan = new Vue({
     el: "#coolZhan",
--- src/js/Home/components/explorezhan.js
+++ src/js/Home/components/explorezhan.js
@@ -1,23 +1,4 @@
-const exploreZhan = [{
-    url: "https://zh.wikipedia.org/",
-    title: "维基百科 | 自由的百科全书",
-}, {
-    url: "https://www.tofugu.com/",
-    title: "Tofugu | A Japanese Culture & Language Blog",
-}, {
-    url: "https://www.naver.com/",
-    title: "네이버 메인에서 다양한 정보와 유용한 컨텐츠를 만나 보세요",
-}, {
-    url: "https://www.wuzuowei.net",
-    title: "无作为 | 分享不仅仅是一种态度！",
-}, {
-    url: "https://www.sigure.tw/",
-    title: "時雨の町 | 日文學習園地",
-}, {
-    url: "https://v.jlszyy.org/",
-    title: "达达兔电影网",
-}]
-
+const { exploreZhan } = require('./data/explorezhan.json')
 
 const explorezhan = new Vue({
     el: "#exploreZhan",
