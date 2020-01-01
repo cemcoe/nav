53e8faac1e9ab94a7e043355c03409adc24bb510
cemcoe <chemxcode@gmail.com>
2020-01-01 12:25:29 +0800

    update navlist

--- src/js/Home/components/siteInfo.js
+++ src/js/Home/components/siteInfo.js
@@ -1,7 +1,7 @@
 const siteinfo = new Vue({
     el: "#footer",
     data: {
-        update: "2019-12-24",
+        update: "2020-01-01",
         email: "admin@cemcoe.com",
         copyright: "ChemCode"
     }
