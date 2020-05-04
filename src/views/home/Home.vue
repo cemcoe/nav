95c7f90592b130781baf256272f683ca53d5e737
cemcoe <chemxcode@gmail.com>
2020-05-04 14:09:45 +0800

    mingzhan

---
+++ src/views/home/Home.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="home">
+    <nav-bar>
+      <div slot="center">Hello Chemcode</div>
+    </nav-bar>
+    <ming-zhan :mingzhan="mingzhan"></ming-zhan>
+  </div>
+</template>
+
+<script>
+import NavBar from "components/common/navbar/NavBar";
+import MingZhan from "./childComps/MingZhan"
+
+import { getHomeMultidata } from "network/home";
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      mingzhan: []
+    };
+  },
+  components: {
+    NavBar,
+    MingZhan
+  },
+  created() {
+    this.getHomeMultidata();
+  },
+  methods: {
+    getHomeMultidata() {
+      getHomeMultidata().then(res => {
+        console.log(res.data.mingzhan);
+        this.mingzhan = res.data.mingzhan;
+      });
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+a {
+  text-decoration: none;
+}
+li {
+  list-style: none;
+}
+</style>
---
+++ src/views/home/childComps/MingZhan.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="mingzhan">
+    <ul>
+      <li v-for="(item, index) in mingzhan" :key="index">
+        <a target="_blank" :href="item.url">{{item.title}}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MingZhan",
+  props: {
+    mingzhan: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.mingzhan ul {
+  display: flex;
+  flex-wrap: wrap;
+  align-content: space-between;
+  background-color: rgb(247, 240, 240);
+}
+.mingzhan li {
+  flex: 0 0 20%;
+  box-sizing: content-box;
+  text-align: center;
+  height: 60px;
+  box-sizing: border-box;
+}
+.mingzhan li:nth-of-type(2n) {
+  background-color: rgb(231, 221, 219);
+}
+.mingzhan li a {
+  color: rgb(3, 3, 3);
+  display: block;
+  width: 100%;
+  height: 100%;
+  line-height: 60px;
+}
+</style>
