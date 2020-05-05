30fd6ee300198dad79feabb4db4b924195de219a
cemcoe <chemxcode@gmail.com>
2020-05-05 16:10:36 +0800

    update pwa info

--- vue.config.js
+++ vue.config.js
@@ -8,5 +8,20 @@ module.exports = {
       }
     }
   },
-  publicPath: '/nav/'
+  publicPath: '/nav/',
+  pwa: {
+    name: '书签',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // 配置 workbox 插件
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // InjectManifest 模式下 swSrc 是必填的。
+      swSrc: 'dev/sw.js',
+      // ...其它 Workbox 选项...
+    }
+  }
 }
