8f1b5cb50933167bcc218b79b1b5e6a7c4ff97b2
cemcoe <chemxcode@gmail.com>
2020-05-05 16:22:18 +0800

    update pwa info

--- vue.config.js
+++ vue.config.js
@@ -15,13 +15,5 @@ module.exports = {
     msTileColor: '#000000',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
-
-    // 配置 workbox 插件
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // InjectManifest 模式下 swSrc 是必填的。
-      swSrc: 'dev/sw.js',
-      // ...其它 Workbox 选项...
-    }
   }
 }
