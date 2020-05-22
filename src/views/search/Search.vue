6f94b7c22aa246dcb5d4f1b974a70e573d08c0e3
cemcoe <chemxcode@gmail.com>
2020-05-22 12:36:19 +0800

    base search

--- src/views/search/Search.vue
+++ src/views/search/Search.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     <van-search v-model="value" placeholder="请输入搜索关键词" />
-    <p>开发中，你要查询的关键词：{{value}}</p>
+    <p>
+      <span @click="kaiClick">开</span>发中，你要查询的关键词：{{value}}
+    </p>
+    <div v-if="isadult">{{pron}}</div>
   </div>
 </template>
 
@@ -9,12 +12,35 @@
 import Vue from "vue";
 import { Search } from "vant";
 
+import { getKeywords } from "network/search";
+
 Vue.use(Search);
 export default {
   name: "Search",
   data() {
     return {
-      value: null
+      value: null,
+      pron: [],
+      open: -6,
+      isadult: false
+    };
+  },
+  created() {
+    this.getKeywords();
+  },
+  methods: {
+    getKeywords() {
+      getKeywords().then(res => {
+        // this.password = res.data.password;
+        this.pron = res.data.pron;
+      });
+    },
+    kaiClick() {
+      console.log("car run");
+      this.open++;
+      if (this.open === 0) {
+        this.isadult = !this.isadult;
+      }
     }
   }
 };
